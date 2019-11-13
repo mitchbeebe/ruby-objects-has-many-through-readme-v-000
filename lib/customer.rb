f862b8c1091065c5385a9fc4809abbb67e2faea4
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 23:26:25 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select { |m| m.customer == self }
+  end
+  
+  def waiters
+    Meal.all.select { |m| m.customer == self }.map{ |i| i.waiter }
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
-
+  
+  attr_accessor :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select { |m| m.waiter == self }
+  end
+  
+  def best_tipper
+    best_tip = 0
+    best_cust = nil
+    Meal.all.each do |m|
+      if m.tip > best_tip
+        best_tip = m.tip
+        best_cust = m.customer
+      end
+    end
+    best_cust
+  end
 end
